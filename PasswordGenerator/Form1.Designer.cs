cfbf90d55f8ddbe13d5e46ed0a25f1046bed6641
Kamil Akhmetzyanov <akhmetzyanov.kamil.00@mail.ru>
2023-05-14 23:49:56 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(179, 468);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(346, 78);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(334, 122);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(179, 175);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(307, 26);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Использовать прописные буквы ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(179, 234);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(295, 26);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Использовать строчные буквы ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.Location = new System.Drawing.Point(179, 294);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(346, 26);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Использовать специальные символы";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox4.Location = new System.Drawing.Point(179, 350);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(208, 26);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Использовать числа ";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(222, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(204, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 35);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Длина";
+          
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(747, 651);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        private const string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string Numbers = "0123456789";
+        private const string SpecialCharacters = "!@#$%^&*()_+-=[]{}|;:,./?><";
+        private List<string> charLists = new List<string>();
+        private readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var pass_len = (int)numericUpDown1.Value;
+            var includeLowercase = checkBox2.Checked;
+            var includeUppercase = checkBox1.Checked;
+            var includeNumbers = checkBox4.Checked;
+            var includeSpecialCharacters = checkBox3.Checked;
+
+            var characterPool = string.Empty;
+
+            if (includeLowercase)
+            {
+                characterPool += LowercaseLetters;
+                charLists.Add(LowercaseLetters);
+            }
+            if (includeUppercase)
+            {
+                characterPool += UppercaseLetters;
+                charLists.Add(UppercaseLetters);
+            }
+            if (includeNumbers)
+            {
+                characterPool += Numbers;
+                charLists.Add(Numbers);
+            }
+            if (includeSpecialCharacters)
+            {
+                characterPool += SpecialCharacters;
+                charLists.Add(SpecialCharacters);
+            }
+
+            if (charLists.Count != 0)
+            {
+                var password = GeneratePassword(pass_len, characterPool);
+                password = GeneratePassword(pass_len, characterPool);
+                richTextBox1.Text = password;
+            }
+        }
+
+
+            public string GeneratePassword(int length, string characterPool)
+            {
+                bool containsAll = false;
+                string password = string.Empty;
+
+                do
+                {
+                    password = string.Empty;
+                    var bytes = new byte[length];
+                    Rng.GetBytes(bytes);
+
+                    for (var i = 0; i < length; i++)
+                    {
+                        var characterIndex = bytes[i] % characterPool.Length;
+                        password += characterPool[characterIndex];
+                    }
+
+                    containsAll = true;
+                    foreach (string list in charLists)
+                    {
+                        if (string.IsNullOrEmpty(list))
+                        {
+                            continue;
+                        }
+
+                        bool containsAtLeastOne = false;
+                        foreach (char c in list)
+                        {
+                            if (password.Contains(c.ToString()))
+                            {
+                                containsAtLeastOne = true;
+                                break;
+                            }
+                        }
+
+                        if (!containsAtLeastOne)
+                        {
+                            containsAll = false;
+                            break;
+                        }
+                    }
+
+                } while (!containsAll);
+
+                return password;
+            }
+        }
+
+}
+        
+    
+
